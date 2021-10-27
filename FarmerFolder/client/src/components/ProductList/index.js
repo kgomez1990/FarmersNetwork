e7811eef9b7c2f6eeda865008f069d52c21210fb
karim gomez <karim.gomez29@yahoo.com>
2021-10-26 20:59:35 -0400

    initial commit

---
+++ FarmerFolder/client/src/components/ProductList/index.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import spinner from '../../assets/spinner.gif';
+
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
+  }
+
+  return (
+    <div className="my-2">
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Products</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Please Enter Item for Sale"></input>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+      <h2>Our Products:</h2>
+      {state.products.length ? (
+        <div className="flex-row">
+          {filterProducts().map((product) => (
+            <ProductItem
+              key={product._id}
+              _id={product._id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      {loading ? <img src={spinner} alt="loading" /> : null}
+    </div>
+  );
+}
+
+export default ProductList;
---
+++ FarmerFolder/server/config/seeds.js
@@ -0,0 +1,155 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+
+db.once('open', async () => {
+  await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: 'Fruit' },
+    { name: 'Vegetables' },
+    { name: 'Meat' },
+    { name: 'HomeMade Bottled Goods' },
+    { name: 'Toys' }
+  ]);
+
+  console.log('categories seeded');
+
+  await Product.deleteMany();
+
+  const products = await Product.insertMany([
+    {
+      name: 'Skirt Steak - 9oz',
+      description:
+        'Antibiotic free Angus Beef Outside Skirt Steak',
+      image: 'Skirt-Steak.jpg',
+      category: categories[0]._id,
+      price: 13.28,
+      quantity: 7
+    },
+    {
+      name: 'Lemons',
+      description:
+        'California Lemons shipped right to your door! Very high in Vitamin C and great for your favorite drink and food recipes. This is a 5lb case(between 12-18 Lemons',
+      image: 'Lemons.jpg',
+      category: categories[0]._id,
+      price: 10.99,
+      quantity: 100
+    },
+    {
+      name: 'Organic Detroit Dark Red Beets - 5LB',
+      category: categories[1]._id,
+      description:
+        'Detroit Dark Red has been the preferred choice for homegrown classic American beets. Introduced in 1892 by Detroit’s world famous D.M. Ferry Seed Company, the American red beet eventually adopted the moniker “Detroit” since it became known that the best beets hailed from the “Motor City”. The 2-3" globe-shaped roots are tender and sweet. A dual-purpose vegetable, the deep green tops can be cooked like swiss chard.',
+      image: 'Beets.jpg',
+      price: 3.13,
+      quantity: 20
+    },
+    {
+      name: 'Cranberry Jam',
+      category: categories[1]._id,
+      description:
+        'Merry, bright and full of festive flavors, this cranberry sauce gets a dose of holiday cheer from citrusy orange zest, warm spices and a splash of brandy. Serve it alongside a celebratory roast, spread it on a turkey sandwich or pair it with a cheese plate!',
+      image: 'Cranberries.jpg',
+      price: 9.99,
+      quantity: 10
+    },
+    {
+      name: 'Set of Wooden Spoons',
+      category: categories[1]._id,
+      description:
+        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+      image: 'wooden-spoons.jpg',
+      price: 14.99,
+      quantity: 100
+    },
+    {
+      name: 'Camera',
+      category: categories[2]._id,
+      description:
+        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+      image: 'camera.jpg',
+      price: 399.99,
+      quantity: 30
+    },
+    {
+      name: 'Tablet',
+      category: categories[2]._id,
+      description:
+        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
+      image: 'tablet.jpg',
+      price: 199.99,
+      quantity: 30
+    },
+    {
+      name: 'Tales at Bedtime',
+      category: categories[3]._id,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+      image: 'bedtime-book.jpg',
+      price: 9.99,
+      quantity: 100
+    },
+    {
+      name: 'Spinning Top',
+      category: categories[4]._id,
+      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
+      image: 'spinning-top.jpg',
+      price: 1.99,
+      quantity: 1000
+    },
+    {
+      name: 'Set of Plastic Horses',
+      category: categories[4]._id,
+      description:
+        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
+      image: 'plastic-horses.jpg',
+      price: 2.99,
+      quantity: 1000
+    },
+    {
+      name: 'Teddy Bear',
+      category: categories[4]._id,
+      description:
+        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
+      image: 'teddy-bear.jpg',
+      price: 7.99,
+      quantity: 100
+    },
+    {
+      name: 'Alphabet Blocks',
+      category: categories[4]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'alphabet-blocks.jpg',
+      price: 9.99,
+      quantity: 600
+    }
+  ]);
+
+  console.log('products seeded');
+
+  await User.deleteMany();
+
+  await User.create({
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+  });
+
+  await User.create({
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345'
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
